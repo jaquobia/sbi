357df19accfcb3cf9d9864e7376ee8bfa05a6751
jaquobia <jaquobia@gmail.com>
2024-02-18 20:27:12 -0500

    Working table expansion

--- Program.cs
+++ Program.cs
@@ -3,8 +3,32 @@ using Spectre.Console;
 
 public static class Program
 {
+
+    static Table table = new Table().Centered();
+
+	public static void start(LiveDisplayContext ctx) {
+		table.AddColumn("Foo");
+		table.AddColumn("Bar");
+		ctx.Refresh();
+		var run = true;
+		while (run) {
+			ctx.Refresh();
+			var key_info = AnsiConsole.Console.Input.ReadKey(true);
+			if (key_info is ConsoleKeyInfo key && key.Key == ConsoleKey.Escape) {
+				run = false;
+			} else {
+				table.AddRow("A");
+			}
+		}
+	}
+
     public static void Main(string[] args)
     {
-        AnsiConsole.Markup("[underline red]Hello[/] World!\n");
-    }
+		// Animate
+		AnsiConsole.Live(table)
+			.AutoClear(true)
+			.Overflow(VerticalOverflow.Ellipsis)
+			.Cropping(VerticalOverflowCropping.Top)
+			.Start(ctx => { start(ctx); });
+	}
 }
