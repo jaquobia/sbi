6ca0d658e4ad277edb8406e91088d8bafe019300
jaquobia <jaquobia@gmail.com>
2025-02-05 20:55:08 -0500

    Begin working on submenus and profile selection.
    Submenu's technically exist but do nothing except for the profile select
    menu.
    Profile selection is in-works but technically functions with the dummy
    list. "Profiles" are listed in rows and can be clicked to be selected.

--- src/application.rs
+++ src/application.rs
@@ -1,4 +1,4 @@
-use iced::Element;
+use iced::{alignment::Vertical, Element, Length::{self, Fill}};
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub enum Executable {
@@ -22,10 +22,20 @@ impl std::fmt::Display for Executable {
     }
 }
 
+#[derive(Default, Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
+enum SubMenu {
+    #[default]
+    SelectProfile,
+    ConfigureProfile,
+    Settings,
+}
+
 #[derive(Default, Debug, Clone)]
 pub struct Application {
     executable_selection: Option<Executable>,
     debug: bool,
+    submenu: SubMenu,
+    selected_profile: Option<usize>,
 }
 
 #[derive(Debug, Clone)]
@@ -33,14 +43,17 @@ pub enum Message {
     SelectExecutable(Executable),
     ButtonSettingsPressed,
     ButtonLaunchPressed,
+    ButtonBackPressed,
     ToggleDebug(bool),
+    SelectProfile(usize),
 }
 
 impl Application {
     pub fn update(&mut self, message: Message) {
         match message {
             Message::ToggleDebug(state) => {
-                self.debug = !self.debug;
+                println!("Toggling debug: {}", state);
+                self.debug = state;
             }
             Message::SelectExecutable(executable) => {
                 println!("Selecting executable: {}", executable);
@@ -48,17 +61,31 @@ impl Application {
             }
             Message::ButtonSettingsPressed => {
                 println!("Settings was pressed");
+                self.submenu = SubMenu::ConfigureProfile;
+            }
+            Message::ButtonBackPressed => {
+                println!("Back...");
+                self.submenu = SubMenu::SelectProfile;
             }
             Message::ButtonLaunchPressed => {
                 println!("Launching starbound...");
+            }
+            Message::SelectProfile(i) => {
+                println!("Selecting profile {}", i);
+                self.selected_profile = Some(i);
             }
         }
     }
 
     pub fn view(&self) -> Element<'_, Message> {
         // Bottom Bar
-        let settings_button_message = Some(Message::ButtonSettingsPressed);
-        let settings_button = iced::widget::button("Settings").on_press_maybe(settings_button_message);
+        let (omni_button_label, omni_button_message) = match self.submenu {
+            SubMenu::SelectProfile => ("Configure Profile", Some(Message::ButtonSettingsPressed)),
+            SubMenu::ConfigureProfile => ("Back", Some(Message::ButtonBackPressed)),
+            SubMenu::Settings => ("Back", Some(Message::ButtonBackPressed)),
+        };
+        let settings_button = iced::widget::button(omni_button_label).on_press_maybe(omni_button_message);
+
         let launch_button_message = Some(Message::ButtonLaunchPressed);
         let launch_button = iced::widget::button("Launch").on_press_maybe(launch_button_message);
         let executable_picker = iced::widget::pick_list(Executable::ALL, self.executable_selection.as_ref(), Message::SelectExecutable).placeholder("Select an executable...");
@@ -66,19 +93,25 @@ impl Application {
         let controls_right = iced::widget::row![
             executable_picker,
             launch_button,
-        ];
+        ].spacing(5);
         let controls = iced::widget::row![
             settings_button,
             debug_checkbox,
             iced::widget::horizontal_space(),
             controls_right,
-        ];
+        ]
+            .spacing(5)
+            .height(40)
+            .align_y(Vertical::Center);
 
         // Profiles Menu
-        let profiles = iced::widget::container("Profiles");
+        let body = match self.submenu {
+            SubMenu::SelectProfile => self.view_select_profile(),
+            SubMenu::ConfigureProfile => self.view_configure_profile(),
+            SubMenu::Settings => self.view_settings(),
+        };
         let content = iced::widget::column![
-            profiles,
-            iced::widget::vertical_space(),
+            body,
             controls,
         ];
 
@@ -90,4 +123,33 @@ impl Application {
             content
         }
     }
+
+    fn view_select_profile(&self) -> Element<'_, Message> {
+        let profile_to_widget = |(i, p)| -> (usize, Element<'_, Message>) {
+            let text_color = self.selected_profile.is_some_and(|p_i| p_i == i).then(|| iced::Color::from_rgba(0.3, 0.7, 0.2, 1.0)); 
+            let text = iced::widget::text!("{}", p).width(Fill).color_maybe(text_color);
+            (
+                i, 
+                iced::widget::mouse_area(text).on_press(Message::SelectProfile(i)).into()
+            )
+        };
+        let profiles = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"];
+        let profiles = iced::widget::keyed_column(profiles.iter().enumerate().map(profile_to_widget))
+            .width(Length::Fill)
+            .align_items(iced::Alignment::Start)
+            .spacing(5);
+        iced::widget::scrollable(profiles)
+            .height(Length::Fill)
+            .spacing(3)
+            .into()
+    }
+
+    fn view_configure_profile(&self) -> Element<'_, Message> {
+        iced::widget::row![].height(Length::Fill).into()
+    }
+
+    fn view_settings(&self) -> Element<'_, Message> {
+        iced::widget::row![].height(Length::Fill).into()
+    }
 }
+
