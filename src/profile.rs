8ff6b66880f8d2b070a0a2b1fefde51af5a31467
jaquobia <jaquobia@gmail.com>
2025-06-16 17:46:14 -0400

    Fix profiles disappearing when json is missing the non-optional link_mods value

--- src/profile.rs
+++ src/profile.rs
@@ -15,6 +15,7 @@ pub struct ProfileJson {
     pub name: String,
     pub additional_assets: Option<Vec<PathBuf>>,
     pub collection_id: Option<String>,
+    #[serde(default)]
     pub link_mods: bool,
 }
 
